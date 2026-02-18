41c8e262937c872beb17ccd23c655c00cc9404be
Paweenkit Thahong <newww@Paweenkits-Laptop.local>
2026-02-18 14:26:54 +0700

    Complete TDD steps for TodoItem

--- frontend/src/Todoitem.jsx
+++ frontend/src/Todoitem.jsx
@@ -1,40 +1,62 @@
 import './App.css'
 import { useState } from 'react'
 
-function TodoItem({todo, toggleDone, deleteTodo, addNewComment}) {
-    const [newComment, setNewComment] = useState("");      // เพิ่ม state newComment
-    return (
-        <li>
-                <span className={todo.done ? "done" : ""}>{todo.title}</span>
-                <button type="button" onClick={() => {toggleDone(todo.id)}}>Toggle</button>
-                <button type="button" onClick={() => {deleteTodo(todo.id)}}>❌</button>
-        {(todo.comments) && (todo.comments.length > 0) && (
-                <>
-                    <b>Comments:</b>
-                    <ul>
-                    {todo.comments.map(comment => (
-                        <li key={comment.id}>{comment.message}</li>
-                    ))}
-        <div className="new-comment-forms">
-                <input
-                    type="text"
-                    value={newComment}
-                    onChange={(e) => {
-                    const value = e.target.value;
-                    setNewComment(value);
-                    }}
-                />
-            <button onClick={() => { 
-            type="button"                        
-            addNewComment(todo.id, newComment);
-            setNewComment("");
-            }}>Add Comment</button>
-            </div>
-                    </ul>
-                </>
-                )}
-            </li>
-    )
-    }
+function TodoItem({ todo, toggleDone, deleteTodo, addNewComment }) {
+  const [newComment, setNewComment] = useState("");
+  const comments = todo.comments || [];
+  const commentCount = comments.length;
+
+  return (
+    <li>
+      <span className={todo.done ? "done" : ""}>
+        {todo.title}
+      </span>
+
+      <button type="button" onClick={() => toggleDone?.(todo.id)}>
+        Toggle
+      </button>
+
+      <button type="button" onClick={() => deleteTodo?.(todo.id)}>
+        ❌
+      </button>
+
+      <p>{commentCount} comments</p>
+
+      {commentCount === 0 && (
+        <p>No comments</p>
+      )}
+
+      {commentCount > 0 && (
+        <>
+          <b>Comments:</b>
+          <ul>
+            {comments.map(comment => (
+              <li key={comment.id}>{comment.message}</li>
+            ))}
+          </ul>
+
+          <div className="new-comment-forms">
+            <input
+              type="text"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+            />
+
+            <button
+              type="button"
+              onClick={() => {
+                addNewComment?.(todo.id, newComment);
+                setNewComment("");
+              }}
+            >
+              Add Comment
+            </button>
+          </div>
+        </>
+      )}
+    </li>
+  )
+}
 
 export default TodoItem
+
